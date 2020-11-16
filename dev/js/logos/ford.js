c8e376675dbe6bdbca5ae4ae9f578613ef9bb7f4
ashleynw1800 <ashleynw1800@gmail.com>
2020-11-16 11:54:28 -0800

    logo-in

--- dev/js/logos/ford.js
+++ dev/js/logos/ford.js
@@ -4,12 +4,11 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-const fordTL = gsap.timeline();
+gsap.set("#ford-logo",{transformOrigin:"center"});
 
-fordTL.from("#ford-out-ring", {
-    duration: 1,
-    drawSVG: "0"
-})
+const fordTL = gsap.timeline();
+//fordTL.to("#ford-logo", {duration:1, alpha:100, scale:6, x:450, y:300})
+    //.from("#ford-out-ring", {duration: 3, drawSVG: "1"});
 
 export function fordAnimation() {
 
