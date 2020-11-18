71955b7fb7cda40b9d3faf6b4f6fbf3034ecc405
ashleynw1800 <ashleynw1800@gmail.com>
2020-11-17 19:58:20 -0800

    logo-reveal

--- dev/js/logos/ford.js
+++ dev/js/logos/ford.js
@@ -4,11 +4,17 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-gsap.set("#ford-logo",{transformOrigin:"center"});
+gsap.set("#ford-fill-stroke-lg",{transformOrigin:"top center"});
 
 const fordTL = gsap.timeline();
-//fordTL.to("#ford-logo", {duration:1, alpha:100, scale:6, x:450, y:300})
-    //.from("#ford-out-ring", {duration: 3, drawSVG: "1"});
+fordTL.from("#ford-fill-stroke-lg", {duration:1, drawSVG: "1", visibility:"visible"})
+        .to("#ford-fill-stroke-lg", {visibility:"visible"})
+        .from("#ford-fill-lg", {duration:0.5, alpha:0, visibility:"visible"}, "stay")
+        .to("#ford-fill-lg", {visibility:"visible"}, "stay")
+        .from("#ford-in-ring-lg", {duration:0.5, alpha:0, visibility:"visible"}, "stay")
+        .to("#ford-in-ring-lg", {visibility:"visible"}, "stay")
+        .from("#ford-name-lg", {duration:0.5, alpha:0, visibility:"visible"}, "stay")
+        .to("#ford-name-lg", {visibility:"visible"}, "stay");
 
 export function fordAnimation() {
 
