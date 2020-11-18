c9439c0729190350303b1b3a8edcdc839ccf58cc
ashleynw1800 <ashleynw1800@gmail.com>
2020-11-17 20:51:53 -0800

    intro

--- dev/js/logos/ford.js
+++ dev/js/logos/ford.js
@@ -14,7 +14,19 @@ fordTL.from("#ford-fill-stroke-lg", {duration:1, drawSVG: "1", visibility:"visib
         .from("#ford-in-ring-lg", {duration:0.5, alpha:0, visibility:"visible"}, "stay")
         .to("#ford-in-ring-lg", {visibility:"visible"}, "stay")
         .from("#ford-name-lg", {duration:0.5, alpha:0, visibility:"visible"}, "stay")
-        .to("#ford-name-lg", {visibility:"visible"}, "stay");
+        .to("#ford-name-lg", {visibility:"visible"}, "stay")
+        
+        .from("#light-1", {duration:1, delay:2, drawSVG:"0%", visibility:"visible"}, {drawSVG:"100%", ease:"linear"}, "+=3")
+        .from("#light-2", {duration:1, delay:2, drawSVG:"0%", visibility:"visible"}, {drawSVG:"100%", ease:"linear"})
+        .from("#light-3", {duration:1, delay:2, drawSVG:"0%", visibility:"visible"}, {drawSVG:"100%", ease:"linear"})
+        .from("#light-4", {duration:1, delay:2, drawSVG:"0%", visibility:"visible"}, {drawSVG:"100%", ease:"linear"})
+        .from("#light-5", {duration:1, delay:2, drawSVG:"0%", visibility:"visible"}, {drawSVG:"100%", ease:"linear"})
+        .to("#white", {visibility:"visible"})
+        .to("#white", {visibility:"hidden"}, "hide")
+        .to("#ford-fill-stroke-lg", {visibility:"hidden"}, "hide")
+        .to("#ford-fill-lg", {visibility:"hidden"}, "hide")
+        .to("#ford-in-ring-lg", {visibility:"hidden"}, "hide")
+        .to("#ford-name-lg", {visibility:"hidden"}, "hide");
 
 export function fordAnimation() {
 
