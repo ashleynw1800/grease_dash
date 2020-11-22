46f005270592540033a92b33a3f8c7f7ce10e75c
ashleynw1800 <ashleynw1800@gmail.com>
2020-11-21 18:46:27 -0800

    center-in

--- dev/js/sequences/buttons.js
+++ dev/js/sequences/buttons.js
@@ -4,11 +4,22 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-gsap.set("#bbbb",{transformOrigin:"center"});
+gsap.set("#song-line-down",{transformOrigin:"center"});
+
 
 const buttonsTL = gsap.timeline();
-buttonsTL.from("#white-speed", {duration:1.5, drawSVG: "0", visibility:"visible"})
-.to("#white-speed", {visibility:"visible"});
+buttonsTL.from("#button-1", {duration:1, y:30, alpha:0, visibility:"visible"}, "buttons")
+.to("#button-1", {visibility:"visible"}, "buttons-stay")
+.from("#button-2", {duration:1.2, y:30, alpha:0, visibility:"visible"}, "buttons")
+.to("#button-2", {visibility:"visible"}, "buttons-stay")
+.from("#button-3", {duration:1.4, y:30, alpha:0, visibility:"visible"}, "buttons")
+.to("#button-3", {visibility:"visible"}, "buttons-stay")
+.from("#button-4", {duration:1.5, y:30, alpha:0, visibility:"visible"}, "buttons")
+.to("#button-4", {visibility:"visible"}, "buttons-stay")
+.from("#song", {duration:1, y:-3, alpha:0, visibility:"visible"})
+.to("#song", {visibility:"visible"})
+.from("#song-line-down", {duration:0.5, drawSVG: "0", visibility:"visible"}, "-=0.3")
+.to("#song-line-down", {visibility:"visible"});
 
 export function buttonsAnimation() {
 
